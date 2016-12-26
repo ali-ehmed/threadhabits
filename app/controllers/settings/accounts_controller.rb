14f8c0025605c0a15d58f9b79e84ded30d87db01
Ali Ahmed <ali_ahmed922@outlook.com>
2016-12-26 15:51:09 +0500

    Old module removed

--- app/controllers/settings/accounts_controller.rb
+++
@@ -1,9 +0,0 @@
-module Settings
-  class AccountsController < BaseController
-    include CommonSettings
-
-    def show
-      PersistenceResponse.new(self, settings_accounts_path, "Your Account Settings has been updated successfully.")
-    end
-  end
-end
