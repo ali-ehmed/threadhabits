32e1f699568bf31c89064972e0e98b620561a59d
Ali Ahmed (New) <ali.ahmed.cs2014@gmail.com>
2016-12-31 17:36:34 +0500

    Registraion fixes 2

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -57,16 +57,16 @@ Rails.application.configure do
   #   :authentication       => 'plain'
   # }
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address              => ENV["smtp_address"],
-  #   :port                 => "587",
-  #   :domain               => ENV["smtp_domain"],
-  #   :user_name            => ENV["smtp_username"],
-  #   :password             => ENV["smtp_password"],
-  #   :authentication       => 'login',
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => ENV["smtp_address"],
+    :port                 => "587",
+    :domain               => ENV["smtp_domain"],
+    :user_name            => ENV["smtp_username"],
+    :password             => ENV["smtp_password"],
+    :authentication       => 'login',
+    :enable_starttls_auto => true
+  }
 
   config.action_mailer.default_url_options = { host: ENV["domain"] }
 
