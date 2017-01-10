5163b3ffba67062cab840bccfc1f410e5e9f6a15
mr-aliahmed <ali.ahmed.cs2014@gmail.com>
2017-01-10 12:53:06 +0500

    Emails css not loading fixes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   #   :enable_starttls_auto => true
   # }
 
-  config.action_mailer.default_url_options = { host: ENV["domain"] }
+  config.action_mailer.default_url_options = { host: ENV["secure_domain"] }
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
