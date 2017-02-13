edeec7e5c4771e1f6fb96b7283e4b84eece85fb4
mr-aliahmed <ali.ahmed.cs2014@gmail.com>
2017-02-13 18:15:51 +0500

    time stamps to persons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118072416) do
+ActiveRecord::Schema.define(version: 20170213131433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,8 @@ ActiveRecord::Schema.define(version: 20170118072416) do
     t.string   "facebook_profile"
     t.string   "instagram_profile"
     t.string   "twitter_profile"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["confirmation_token"], name: "index_people_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_people_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true, using: :btree
